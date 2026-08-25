b9f32740aaa91d5251d81980457f28dab6429562
pranay-parmarketing <pranay.badgujar@parmarketing.agency>
2026-08-25 16:04:44 +0530

    api

--- services/api.js
+++ services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-    // baseURL: "https://social-backend.parmarketing.co.uk/api",
-    baseURL: "http://localhost:8019/api",
+    baseURL: "https://social-backend.parmarketing.co.uk/api",
+    // baseURL: "http://localhost:8019/api",
 });
 
 export default API;
